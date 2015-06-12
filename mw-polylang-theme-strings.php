41f9bd2c2c6fd84ad0ada370968ceeca296fe718
modeewine <modeewine@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-12 15:37:27 +0000

    New version (1.1) * Fixed bug when removing the plugin from the admin panel. * Improved code.
    git-svn-id: https://plugins.svn.wordpress.org/polylang-theme-strings/tags/1.1@1179715 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mw-polylang-theme-strings.php
+++ mw-polylang-theme-strings.php
@@ -4,7 +4,7 @@
     Plugin Name: Polylang Theme Strings
     Plugin URI: http://modeewine.com/en-polylang-theme-strings
     Description: Automatic scanning of strings translation in the theme and registration of them in Polylang plugin.
-    Version: 1.0
+    Version: 1.1
     Author: Modeewine
     Author URI: http://modeewine.com
     License: GPL2
